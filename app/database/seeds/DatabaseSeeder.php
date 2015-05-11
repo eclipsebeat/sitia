dcd7befa8f7d6519c7de8a2e06a801b8fc7096c7
eclipsebeat <eclipsebeat@gmail.com>
2015-05-11 20:28:46 +0700

    twitter bootstrap dan master/partial layouts

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -160,7 +160,7 @@ class ArsipTableSeeder extends Seeder {
         for ($i = 0; $i < 10; $i++)
 		{
 		  Arsip::create(array(
-		  	'arsip' => $faker->words($nb = 5),
+		  	'arsip' => $faker->word($nb = 5),
 		    'jenis_arsip_id' => $faker->numberBetween($min = 1, $max = 10),
 			'gudang_id' => $faker->numberBetween($min = 1, $max = 10),
 			'rak_id' => $faker->numberBetween($min = 1, $max = 10),
@@ -182,10 +182,10 @@ class KantorTableSeeder extends Seeder {
 		{
 		  Kantor::create(array(
 		  	'kanwil_id' => $faker->numberBetween($min = 1, $max = 10),
-		  	'kantor' => $faker->words,
-			'alamat' => $faker->words,
-			'telpon' => $faker->words,
-			'fax' => $faker->words,
+		  	'kantor' => $faker->word,
+			'alamat' => $faker->word,
+			'telpon' => $faker->word,
+			'fax' => $faker->word,
 			'email' => $faker->email,
 		));
 		}
