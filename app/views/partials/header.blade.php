ef4b75da02874dde585241af7abd56d93f50deb4
mashtree <mashtree@gmail.com>
2015-05-18 05:52:14 +0700

    komponen menu
    bisa dirubah sesuai kebutuhan

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -18,10 +18,15 @@
 							<li><a href="{{ url('/arsip/rekam') }}">Rekam Arsip</a></li>
 						</ul>
 					</li>
+					<li >
+						<ul><li>Pencarian</li></ul>
+					</li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Utility<span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
 								<li><a href="{{ url('/Utility') }}">RUH User</a></li>
+								<li><a href="{{ url('/Utility') }}">RUH Lokasi Arsip</a></li>
+								<li><a href="{{ url('/Utility') }}">RUH Jenis Arsip</a></li>
 								<li><a href="{{ url('/auth/logout') }}">Backup dan Restore</a></li>
 							</ul>
 						</li>
